42324a1828057f9c188ab6405a42b41277c9a143
a-ilawy <mahmed20156947@gmail.com>
2025-04-28 00:13:47 +0300

    feat(auth): add login||logout||signup functionality

--- AI_Blog_Generator/urls.py
+++ AI_Blog_Generator/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  
-    path('',views.home,name="home")
+    path('',views.home,name="home"),
+    path('login/',views.loginView, name="login"),
+    path('signup/',views.signup, name="signup"),
+    path('logout/',views.logoutView, name="logout")
 ]
 
--- AI_Blog_Generator/views.py
+++ AI_Blog_Generator/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def home(request):
-   
     return render(request, 'index.html',{})
+
+def loginView(request):
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request=request,username=username, password=password)
+        print(user)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+             return render(request, 'signup.html',{'err_msg':"Invalid Username or password"})
+    return render(request, 'login.html',{})
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        repearPass = request.POST.get('repeatpassword')
+        print(f"username: {username} || email: {email} || password: {password} || repearPass: {repearPass} || ")
+        if password!=repearPass:
+             return render(request, 'signup.html',{'err_msg':"The password does not match."})
+        
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            login(request, user)
+            return redirect('/')
+        except:
+            return render(request, 'signup.html',{'err_msg':"Error creating account"})
+    
+    return render(request, 'signup.html',{})
+
+def logoutView(request):
+    logout(request=request)
+    return redirect('/login')
+   
